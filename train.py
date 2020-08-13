25c5b353325744cab553a029ebb6e728ca9e74f1
Qianying Wang <w.qianying@outlook.com>
2020-08-13 22:37:29 +0100

    tensor type

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ Created on Fri Aug  7 21:30:49 2020
 """
 
 
-
+import os
 from tqdm import tqdm
 
 
@@ -15,11 +15,13 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from utils import get_ans_idx, metric_em, metric_f1
+
+import utils
+
 
 
 #%%
-def train(model, iterator, criterion, optimizer, scheduler, metrics, clip, accum_step, threshold):
+def train(model, iterator, optimizer, scheduler, clip, accum_step):
     
     scores = {'loss': 0}
     len_iter = len(iterator)
@@ -28,17 +30,24 @@ def train(model, iterator, criterion, optimizer, scheduler, metrics, clip, accum
     optimizer.zero_grad()
     
     with tqdm(total=len_iter) as progress_bar:      
-        for i, batch in enumerate(iterator):
-                       
+        for i, batch in enumerate(iterator):                      
+            
+            p1s, p2s = model(batch.context, batch.question) 
             # batch.context: [batch_size, c_len]
             # batch.question: [batch_size, q_len]
             # batch.y1s, batch.y2s: [batch_size, 3]
-            p1s, p2s = model(batch.context, batch.question) 
             # p1s, p2s: [batch_size, c_len]
             
             # Take the 1st match as true answer
-            y1, y2 = batch.y1s[:,0], batch.y2s[:,0]
-            loss = F.nll_loss(p1s, y1) + F.nll_loss(p2s, y2)
+            y1s, y2s = [], []
+            for y1, y2 in zip(batch.y1s, batch.y2s):
+                y1s.append([y1[0]])
+                y2s.append([y2[0]])
+            y1s = torch.tensor(y1s).squeeze(1) 
+            y2s = torch.tensor(y2s).squeeze(1)  
+            
+            # y1, y2 = batch.y1s[:,0], batch.y2s[:,0]
+            loss = F.nll_loss(p1s, y1s) + F.nll_loss(p2s, y2s)
                        
             scores['loss'] += loss.item() 
             # # Average loss of multiple answers
@@ -59,7 +68,6 @@ def train(model, iterator, criterion, optimizer, scheduler, metrics, clip, accum
                 optimizer.step()
                 scheduler.step()
                 optimizer.zero_grad()
-            
             progress_bar.update(1)  # update progress bar             
     
     for key, value in scores.items():
@@ -69,7 +77,7 @@ def train(model, iterator, criterion, optimizer, scheduler, metrics, clip, accum
 
 
 #%%
-def evaluate(model, MND, iterator, criterion, optimizer, scheduler, metrics, clip, accum_step, threshold):
+def evaluate(model, MND, iterator):
     
     scores = {'loss': 0, 'em': 0, 'f1': 0}
     len_iter = len(iterator)
@@ -78,42 +86,54 @@ def evaluate(model, MND, iterator, criterion, optimizer, scheduler, metrics, cli
     
     with torch.no_grad():
         with tqdm(total=len_iter) as progress_bar:      
-            for i, batch in enumerate(iterator):
+            for batch in iterator:
                            
                 p1s, p2s = model(batch.context, batch.question)  # [batch_size, c_len]
                 
                 # Take the 1st match as true answer
-                y1, y2 = batch.y1s[:,0], batch.y2s[:,0]  # [batch_size]
-                loss = F.nll_loss(p1s, y1) + F.nll_loss(p2s, y2)
+                y1s, y2s = [], []
+                for y1, y2 in zip(batch.y1s, batch.y2s):
+                    y1s.append([y1[0]])
+                    y2s.append([y2[0]])
+                y1s = torch.tensor(y1s).squeeze(1)  
+                y2s = torch.tensor(y2s).squeeze(1)   
+            
+                # y1, y2 = batch.y1s[:,0], batch.y2s[:,0]  # [batch_size]
+                loss = F.nll_loss(p1s, y1s) + F.nll_loss(p2s, y2s)
                  
                 # Get start/end idxs
                 p1s, p2s = p1s.exp(), p1s.exp()
-                s_idxs, e_idxs = get_ans_idx(p1s, p2s)  # [batch_size]
+                s_idxs, e_idxs = utils.get_ans_idx(p1s, p2s)  # [batch_size]
     
                 # Convert idxs to tokens   
                 batch_em, batch_f1 = 0, 0
-                for i, (con, s_pred, e_pred, s_true, e_true) in enumerate(zip(batch.context, s_idxs, e_idxs, y1, y2)):
+                for i, (con, s_pred, e_pred, s_true, e_true) in enumerate(zip(batch.context, s_idxs, e_idxs, y1s, y2s)):
+                    
+                    s_pred = s_pred.item()
+                    e_pred = e_pred.item()
+                    s_true = s_true.item()
+                    e_true = e_true.item()
                     
                     # True tokens
                     ans_tokens_true = []                    
-                    ans_vocab_idx_true = con[s_true[i]:e_true[i]]  # "idx of content" =>>> "idx of TEXT vocab"
+                    ans_vocab_idx_true = con[s_true:e_true]  # "idx of content" =>>> "idx of TEXT vocab"
                     for vocab_idx in ans_vocab_idx_true:
                         text = MND.TEXT.vocab.itos[vocab_idx]
                         ans_tokens_true.append(text.lower())
                         
                     # Pred tokens
                     ans_tokens_pred = []
-                    ans_vocab_idx_pred = con[s_pred[i]:e_pred[i]]  # "idx of content" =>>> "idx of TEXT vocab"
+                    ans_vocab_idx_pred = con[s_pred:e_pred]  # "idx of content" =>>> "idx of TEXT vocab"
                     for vocab_idx in ans_vocab_idx_pred:
                         text = MND.TEXT.vocab.itos[vocab_idx]
                         ans_tokens_pred.append(text.lower())
                                                     
-                    batch_em += metric_em(ans_tokens_pred, ans_tokens_true)
-                    batch_f1 += metric_f1(ans_tokens_pred, ans_tokens_true)    
+                    batch_em += utils.metric_em(ans_tokens_pred, ans_tokens_true)
+                    batch_f1 += utils.metric_f1(ans_tokens_pred, ans_tokens_true)    
                         
                 scores['loss'] += loss.item()           
-                scores['em'] += batch_em / len(y1)
-                scores['f1'] += batch_f1 / len(y1)
+                scores['em'] += batch_em / batch.context.shape[0]
+                scores['f1'] += batch_f1 / batch.context.shape[0]
                 
                 progress_bar.update(1)  # update progress bar             
     
@@ -121,3 +141,5 @@ def evaluate(model, MND, iterator, criterion, optimizer, scheduler, metrics, cli
         scores[key] = value / len_iter
     
     return scores
+
+
