0359c2fe926446e3f9f06a04a6cbeef3f22a14fc
Qianying Wang <w.qianying@outlook.com>
2020-11-05 22:02:03 +0000

    simplified

--- bidaf/train.py
+++ bidaf/train.py
@@ -7,7 +7,6 @@ Created on Fri Aug  7 21:30:49 2020
 """
 
 
-import os
 from tqdm import tqdm
 
 
@@ -21,9 +20,9 @@ import utils
 
 
 #%%
-def train(model, MND, iterator, optimizer, scheduler, clip, accum_step):
+def train_fn(model, BaseIter, iterator, optimizer, scheduler, clip, accum_step):
     
-    scores = {'loss': 0, 'em': 0, 'f1': 0}
+    scores = {'loss': 0, 'em': 0, 'f1': 0, 'prec': 0, 'rec': 0}
     len_iter = len(iterator)
     n_samples = 0
     
@@ -31,23 +30,20 @@ def train(model, MND, iterator, optimizer, scheduler, clip, accum_step):
     optimizer.zero_grad()
     
     with tqdm(total=len_iter) as progress_bar:      
-        for i, batch in enumerate(iterator):                      
+        for j, batch in enumerate(iterator):                      
             
-            p1s, p2s = model(batch.context, batch.question) 
             # batch.context: [batch_size, c_len]
             # batch.question: [batch_size, q_len]
-            # batch.y1s, batch.y2s: [batch_size, 3]
+            # batch.y1s, batch.y2s: list. len = batch_size
             # p1s, p2s: [batch_size, c_len]
             
-            # Take the 1st match as true answer
-            y1s, y2s = [], []
-            for y1, y2 in zip(batch.y1s, batch.y2s):
-                y1s.append([y1[0]])
-                y2s.append([y2[0]])
-            y1s = torch.tensor(y1s).squeeze(1) 
-            y2s = torch.tensor(y2s).squeeze(1)  
+            # Get start/end idxs
+            p1s, p2s = model(batch.context, batch.question)
+            p1s, p2s = p1s.exp(), p1s.exp()
+            s_idxs, e_idxs = utils.get_ans_idx(p1s, p2s)  # [batch_size]
+                     
+            y1s, y2s = torch.LongTensor(batch.y1s), torch.LongTensor(batch.y2s)
             
-            # y1, y2 = batch.y1s[:,0], batch.y2s[:,0]
             loss = F.nll_loss(p1s, y1s) + F.nll_loss(p2s, y2s)
             scores['loss'] += loss.item() 
              
@@ -56,40 +52,42 @@ def train(model, MND, iterator, optimizer, scheduler, clip, accum_step):
             nn.utils.clip_grad_norm_(model.parameters(), clip)  # prevent exploding gradients
                       
             # Gradient accumulation    
-            if (i+1) % accum_step == 0:
+            if (j+1) % accum_step == 0:
                 optimizer.step()
                 scheduler.step()
                 optimizer.zero_grad()
-                
-            # Get start/end idxs
-            p1s, p2s = p1s.exp(), p1s.exp()
-            s_idxs, e_idxs = utils.get_ans_idx(p1s, p2s)  # [batch_size]
+                           
             
-            for _, (con, s_pred, e_pred, s_true, e_true) in enumerate(zip(batch.context, s_idxs, e_idxs, y1s, y2s)):  
-                
-                s_pred, e_pred = s_pred.item(), e_pred.item()
-                s_true, e_true = s_true.item(), e_true.item()                    
-                # Convert answer idxs to tokens
-                ans_tokens_true = utils.ans_idx_to_tokens(con, s_true, e_true, MND)
-                ans_tokens_pred = utils.ans_idx_to_tokens(con, s_pred, e_pred, MND)
-                # Calculate em/f1 for each record
-                scores['em'] += utils.metric_em(ans_tokens_pred, ans_tokens_true)
-                scores['f1'] += utils.metric_f1(ans_tokens_pred, ans_tokens_true)   
-                n_samples += 1      
+            for i in range(p1s.shape[0]):                                              
+                if (y1s[i] >= 0) and (y2s[i] >= 0):  # When the answer passage not truncated
+                    context_idxs = batch.context[i]  
+                    # Convert answer idxs to tokens
+                    ans_tokens_true = utils.ans_idx_to_tokens(context_idxs, y1s[i], y2s[i], BaseIter)
+                    ans_tokens_pred = utils.ans_idx_to_tokens(context_idxs, s_idxs[i].item(), e_idxs[i].item(), BaseIter)
+                    
+                    scores['em'] += utils.metric_em(ans_tokens_pred, ans_tokens_true)
+                    f1, prec, rec = utils.metric_f1_pr(ans_tokens_pred, ans_tokens_true) 
+                    scores['f1'] += f1
+                    scores['prec'] += prec
+                    scores['rec'] += rec
+                    
+                n_samples += 1   
                 
             progress_bar.update(1)  # update progress bar             
             
     scores['loss'] = scores['loss'] / len_iter
     scores['em'] = scores['em'] / n_samples
     scores['f1'] = scores['f1'] / n_samples
+    scores['prec'] = scores['prec'] / n_samples
+    scores['rec'] = scores['rec'] / n_samples
 
     return scores
 
 
 #%%
-def evaluate(model, MND, iterator):
+def valid_fn(model, BaseIter, iterator):
     
-    scores = {'loss': 0, 'em': 0, 'f1': 0}
+    scores = {'loss': 0, 'em': 0, 'f1': 0, 'prec': 0, 'rec': 0}
     len_iter = len(iterator)
     n_samples = 0
     
@@ -98,44 +96,36 @@ def evaluate(model, MND, iterator):
     with torch.no_grad():
         with tqdm(total=len_iter) as progress_bar:      
             for batch in iterator:
-                           
-                p1s, p2s = model(batch.context, batch.question)  # [batch_size, c_len]
-                
-                # Take the 1st match as true answer
-                y1s, y2s = [], []
-                for y1, y2 in zip(batch.y1s, batch.y2s):
-                    y1s.append([y1[0]])
-                    y2s.append([y2[0]])
-                y1s = torch.tensor(y1s).squeeze(1)  
-                y2s = torch.tensor(y2s).squeeze(1)   
-            
-                # y1, y2 = batch.y1s[:,0], batch.y2s[:,0]  # [batch_size]
-                loss = F.nll_loss(p1s, y1s) + F.nll_loss(p2s, y2s)
-                 
-                # Get start/end idxs
+                # Get start/end idxs          
+                p1s, p2s = model(batch.context, batch.question)  # [batch_size, c_len]                
                 p1s, p2s = p1s.exp(), p1s.exp()
                 s_idxs, e_idxs = utils.get_ans_idx(p1s, p2s)  # [batch_size]
-    
-                for _, (con, s_pred, e_pred, s_true, e_true) in enumerate(zip(batch.context, s_idxs, e_idxs, y1s, y2s)):
-                    
-                    s_pred, e_pred = s_pred.item(), e_pred.item()
-                    s_true, e_true = s_true.item(), e_true.item()
-                    
-                    # Convert answer idxs to tokens
-                    ans_tokens_true = utils.ans_idx_to_tokens(con, s_true, e_true, MND)
-                    ans_tokens_pred = utils.ans_idx_to_tokens(con, s_pred, e_pred, MND)
-                    # Calculate em/f1 for each record
-                    scores['em'] += utils.metric_em(ans_tokens_pred, ans_tokens_true)
-                    scores['f1'] += utils.metric_f1(ans_tokens_pred, ans_tokens_true)   
-                    n_samples += 1
-                                                                                               
-                scores['loss'] += loss.item()                          
+                
+                y1s, y2s = torch.LongTensor(batch.y1s), torch.LongTensor(batch.y2s)
+            
+                loss = F.nll_loss(p1s, y1s) + F.nll_loss(p2s, y2s)
+                scores['loss'] += loss.item()  
+                
+                for i in range(p1s.shape[0]):                                              
+                    if (y1s[i] >= 0) and (y2s[i] >= 0):  # When the answer passage not truncated
+                        context_idxs = batch.context[i]  
+                        # Convert answer idxs to tokens
+                        ans_tokens_true = utils.ans_idx_to_tokens(context_idxs, y1s[i], y2s[i], BaseIter)
+                        ans_tokens_pred = utils.ans_idx_to_tokens(context_idxs, s_idxs[i].item(), e_idxs[i].item(), BaseIter)
+                        
+                        scores['em'] += utils.metric_em(ans_tokens_pred, ans_tokens_true)
+                        f1, prec, rec = utils.metric_f1_pr(ans_tokens_pred, ans_tokens_true) 
+                        scores['f1'] += f1
+                        scores['prec'] += prec
+                        scores['rec'] += rec
+        
+                    n_samples += 1                                                                                                                     
                 progress_bar.update(1)  # update progress bar 
                 
     scores['loss'] = scores['loss'] / len_iter
     scores['em'] = scores['em'] / n_samples
     scores['f1'] = scores['f1'] / n_samples
+    scores['prec'] = scores['prec'] / n_samples
+    scores['rec'] = scores['rec'] / n_samples
 
     return scores
-
-
