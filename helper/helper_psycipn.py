19c6596c6a7d9513ffeb1b7bc85d685cce876597
Qianying Wang <w.qianying@outlook.com>
2020-11-05 22:00:02 +0000

    modify for iterators

--- helper/helper_psycipn.py
+++ helper/helper_psycipn.py
@@ -9,6 +9,7 @@ Created on Fri Oct 30 16:49:24 2020
 
 import json
 import re
+import os
 
 # import torch
 # from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
@@ -88,7 +89,7 @@ def add_ans_char_pos(data_list, max_n_sent):
         record = {"QuesID": ls["QuesID"],
                   "PubID": ls['PubID'],
                   "title": ls['title'],
-                  "question": ques,
+                  "question": ques,  # ques_new is only used for sentences retrieval
                   "answers": [{"text": ls['answer'], "answer_start": int(ans_start)}],
                   "context": context,
                   "group": ls['group']}
@@ -98,18 +99,103 @@ def add_ans_char_pos(data_list, max_n_sent):
 
 
 #%%
-with open('/media/mynewdrive/bioqa/PsyCIPN-InduceIntervene-796-factoid-30102020.json') as fin:
-    dat = json.load(fin)   
+# with open('/media/mynewdrive/bioqa/PsyCIPN-InduceIntervene-796-factoid-30102020.json') as fin:
+#     dat = json.load(fin)   
     
 
-dat_new = add_ans_char_pos(dat, max_n_sent=20)  # 33mins
-with open("/media/mynewdrive/bioqa/PsyCIPN-II-796-factoid-20s-02112020.json", 'w') as fout:
-    fout.write(json.dumps(dat_new))
+# dat_new = add_ans_char_pos(dat, max_n_sent=20)  # 33mins
+# with open("/media/mynewdrive/bioqa/PsyCIPN-II-796-factoid-20s-02112020.json", 'w') as fout:
+#     fout.write(json.dumps(dat_new))
 
-dat_new = add_ans_char_pos(dat, max_n_sent=40)
-with open("/media/mynewdrive/bioqa/PsyCIPN-II-796-factoid-40s-02112020.json", 'w') as fout:
-    fout.write(json.dumps(dat_new))
+# dat_new = add_ans_char_pos(dat, max_n_sent=40)
+# with open("/media/mynewdrive/bioqa/PsyCIPN-II-796-factoid-40s-02112020.json", 'w') as fout:
+#     fout.write(json.dumps(dat_new))
     
-dat_new = add_ans_char_pos(dat, max_n_sent=60)  # 33mins
-with open("/media/mynewdrive/bioqa/PsyCIPN-II-796-factoid-60s-02112020.json", 'w') as fout:
-    fout.write(json.dumps(dat_new))
+# dat_new = add_ans_char_pos(dat, max_n_sent=60)  # 33mins
+# with open("/media/mynewdrive/bioqa/PsyCIPN-II-796-factoid-60s-02112020.json", 'w') as fout:
+#     fout.write(json.dumps(dat_new))
+    
+    
+#%% For torchtext iterators (baseline models)
+def convert_idx(text, tokens):
+    """
+        List of tuples for each token: (token_start_char_idx, token_end_char_idx)     
+    """
+    current = 0
+    spans = []
+    for token in tokens:
+        current = text.find(token, current)  # Find position of 1st occurrence; start search from 'current' 
+        if current < 0:
+            raise Exception(f"Token '{token}' cannot be found")
+        spans.append((current, current + len(token)))
+        current += len(token)  # next search start from the token afterwards
+    return spans
+
+
+def process_for_baseline(dat):
+    """ Obtain answer token idxs from 'answer_start'
+    """
+    records = []
+    count = 0
+    for record in dat:
+        # Context
+        context = record['context'].replace("''", '" ').replace("``", '" ') # Replace non-standard quotation marks
+        context_tokens = text2tokens(context)
+        spans = convert_idx(context, context_tokens)  # (token_start_char_idx, token_end_char_idx)
+        
+        # Question
+        ques = record['question'].replace("''", '" ').replace("``", '" ')  
+        ques_tokens = text2tokens(ques)
+        
+        # Answers   
+        ans_text = record['answers'][0]['text'][0]
+        ans_start = record['answers'][0]['answer_start']
+        # Convert start/end_char_idxs to start/end_token_idxs
+        if ans_start != -999:
+            ans_end = ans_start + len(ans_text) - 1            
+            ans_token_idxs = []
+            for token_idx, span in enumerate(spans):
+                if span[0] < ans_end and span[1] > ans_start:
+                    ans_token_idxs.append(token_idx)
+            y1s, y2s = ans_token_idxs[0], ans_token_idxs[-1]    
+        else:
+            y1s, y2s = -999, -999
+                
+        
+        res_dict = {"context_tokens": context_tokens,
+                    "ques_tokens": ques_tokens,
+                    "answer": ans_text,
+                    "y1s": y1s,
+                    "y2s": y2s,
+                    "id": count,
+                    "group": record['group']}
+        count += 1
+        records.append(res_dict)
+    
+    return records
+
+#%%
+# # factoid-20sents
+# data_path = "/media/mynewdrive/bioqa/PsyCIPN-II-796-factoid-20s-02112020.json"  
+# with open(data_path) as fin:
+#     dat = json.load(fin)
+
+# dat_train, dat_valid, dat_test = [], [], []
+# for ls in dat:
+#     if ls['group'] == 'train':
+#         dat_train.append(ls)
+#     elif ls['group'] == 'valid':
+#         dat_valid.append(ls)
+#     else:
+#         dat_test.append(ls)
+
+# train_processed = process_for_baseline(dat_train)
+# valid_processed = process_for_baseline(dat_valid)
+# test_processed = process_for_baseline(dat_test)    
+    
+# with open(os.path.join(os.path.dirname(data_path), 'train.json'), 'w') as fout:
+#     fout.write(json.dumps(train_processed))
+# with open(os.path.join(os.path.dirname(data_path), 'valid.json'), 'w') as fout:
+#     fout.write(json.dumps(valid_processed))        
+# with open(os.path.join(os.path.dirname(data_path), 'test.json'), 'w') as fout:
+#     fout.write(json.dumps(test_processed)) 
