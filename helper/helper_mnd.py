7650f119e79092f363237b050f86fc64aceda045
Qianying Wang <w.qianying@outlook.com>
2020-11-05 21:59:23 +0000

    1st y1/y2

--- helper/helper_mnd.py
+++ helper/helper_mnd.py
@@ -13,11 +13,8 @@ import os
 nlp = spacy.load("en_core_web_sm")
 
 #%%
-def word_tokenizer(text, lower=True):
-    if lower:
-        tokens = [token.text.lower() for token in nlp(text)]
-    else:
-        tokens = [token.text for token in nlp(text)]
+def text2tokens(text):
+    tokens = [token.text for token in nlp(text)]
     return tokens
 
 def convert_idx(text, tokens):
@@ -35,43 +32,38 @@ def convert_idx(text, tokens):
     return spans
 
 
-def process_records(dat, lower):
+def process_for_baseline(dat):
     
     records = []
     count = 0
     for record in dat:
         # Context
         context = record['context'].replace("''", '" ').replace("``", '" ') # Replace non-standard quotation marks
-        context_tokens = word_tokenizer(context, lower)
-        # (token_start_char_idx, token_end_char_idx)     
-        spans = convert_idx(context, context_tokens)
+        context_tokens = text2tokens(context)       
+        spans = convert_idx(context, context_tokens)  # (token_start_char_idx, token_end_char_idx)
         
         # Question
         ques = record['question'].replace("''", '" ').replace("``", '" ')  
-        ques_tokens = word_tokenizer(ques, lower)
+        ques_tokens = text2tokens(ques)
         
         # Answers   
-        y1s, y2s = [], []
-        answers = []
-        
-        for ans in record['answers']:
-            ans_text = ans['text']
-            ans_start = ans['answer_start']
-            if ans_start != -999:
-                ans_end = ans_start + len(ans_text) - 1
-                answers.append(ans_text)
-                
-                ans_token_idxs = []
-                for token_idx, span in enumerate(spans):
-                    if span[0] < ans_end and span[1] > ans_start:
-                        ans_token_idxs.append(token_idx)
-                y1, y2 = ans_token_idxs[0], ans_token_idxs[-1]    
-                y1s.append(y1)
-                y2s.append(y2)
+        ans_text = record['answers'][0]['text']
+        ans_start = record['answers'][0]['answer_start']
+        # Convert start/end_char_idxs to start/end_token_idxs
+        if ans_start != -999:
+            ans_end = ans_start + len(ans_text) - 1          
+            ans_token_idxs = []
+            for token_idx, span in enumerate(spans):
+                if span[0] < ans_end and span[1] > ans_start:
+                    ans_token_idxs.append(token_idx)
+            y1s, y2s = ans_token_idxs[0], ans_token_idxs[-1]    
+        else:
+            y1s, y2s = -999, -999
+
         
         res_dict = {"context_tokens": context_tokens,
                     "ques_tokens": ques_tokens,
-                    "answer": answers[0],
+                    "answer": ans_text,
                     "y1s": y1s,
                     "y2s": y2s,
                     "id": count}
@@ -81,44 +73,43 @@ def process_records(dat, lower):
     return records
 
 #%%
-# Intervention only
-json_dir = '/media/mynewdrive/bioqa/mnd/intervention'
-with open(os.path.join(json_dir, 'MND-Intervention-1983-06Aug20.json')) as fin:
-    dat = json.load(fin)
+# # Intervention only
+# json_dir = '/media/mynewdrive/bioqa/mnd/intervention'
+# with open(os.path.join(json_dir, 'MND-Intervention-1983-06Aug20.json')) as fin:
+#     dat = json.load(fin)
 
-# Disease only
-json_dir = '/media/mynewdrive/bioqa/mnd/disease'
-with open(os.path.join(json_dir, 'MND-Disease-1950-19Aug20.json')) as fin:
-    dat = json.load(fin)
+# # Disease only
+# json_dir = '/media/mynewdrive/bioqa/mnd/disease'
+# with open(os.path.join(json_dir, 'MND-Disease-1950-19Aug20.json')) as fin:
+#     dat = json.load(fin)
     
-# Intervention & Disease
-json_dir = '/media/mynewdrive/bioqa/mnd/di'
-with open(os.path.join(json_dir, 'MND-DI-3933-19Aug20.json')) as fin:
-    dat = json.load(fin)
+# # Intervention & Disease
+# json_dir = '/media/mynewdrive/bioqa/mnd/di'
+# with open(os.path.join(json_dir, 'MND-DI-3933-19Aug20.json')) as fin:
+#     dat = json.load(fin)
 
 
-train_ls = process_records(dat['train'], lower=False)
-for i, ls in enumerate(train_ls):
-    train_ls[i]['id'] = 'train_' + str(train_ls[i]['id'])
+# train_ls = process_records(dat['train'], lower=False)
+# for i, ls in enumerate(train_ls):
+#     train_ls[i]['id'] = 'train_' + str(train_ls[i]['id'])
     
-valid_ls = process_records(dat['valid'], lower=False)
-for i, ls in enumerate(valid_ls):
-    valid_ls[i]['id'] = 'valid_' + str(valid_ls[i]['id'])
+# valid_ls = process_records(dat['valid'], lower=False)
+# for i, ls in enumerate(valid_ls):
+#     valid_ls[i]['id'] = 'valid_' + str(valid_ls[i]['id'])
     
-test_ls = process_records(dat['test'], lower=False)
-for i, ls in enumerate(test_ls):
-    test_ls[i]['id'] = 'test_' + str(test_ls[i]['id'])
+# test_ls = process_records(dat['test'], lower=False)
+# for i, ls in enumerate(test_ls):
+#     test_ls[i]['id'] = 'test_' + str(test_ls[i]['id'])
     
     
-
-with open(os.path.join(json_dir, 'train.json'), 'w') as fout:
-    for l in train_ls:     
-        fout.write(json.dumps(l) + '\n')  
+# with open(os.path.join(json_dir, 'train.json'), 'w') as fout:
+#     for l in train_ls:     
+#         fout.write(json.dumps(l) + '\n')  
         
-with open(os.path.join(json_dir, 'valid.json'), 'w') as fout:
-    for l in valid_ls:     
-        fout.write(json.dumps(l) + '\n')   
+# with open(os.path.join(json_dir, 'valid.json'), 'w') as fout:
+#     for l in valid_ls:     
+#         fout.write(json.dumps(l) + '\n')   
 
-with open(os.path.join(json_dir, 'test.json'), 'w') as fout:
-    for l in test_ls:     
-        fout.write(json.dumps(l) + '\n')   
+# with open(os.path.join(json_dir, 'test.json'), 'w') as fout:
+#     for l in test_ls:     
+#         fout.write(json.dumps(l) + '\n')   
