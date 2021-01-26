7fc3912bb6efe3f20632e7c60ba39f87597b9e7b
Qianying Wang <w.qianying@outlook.com>
2021-01-26 13:05:32 +0000

    ls mod

--- bert/run_bert.py
+++ bert/run_bert.py
@@ -8,6 +8,7 @@ Created on Wed Aug 26 15:26:38 2020
 
 import json
 import os
+os.chdir('/home/qwang/bioqa')
 import random
 
 from transformers import BertTokenizerFast, BertForQuestionAnswering
@@ -21,7 +22,7 @@ from torch.utils.data import DataLoader
 from arg_parser import get_args
 import utils
 from bert.bert_fn import read_data, char2token_encodings, EncodingDataset
-from bert.bert_fn import train_fn, valid_fn
+from bert.bert_fn import train_fn, valid_fn, train_fn_list, valid_fn_list
 
 #%% Read data
 args = get_args()
@@ -35,26 +36,33 @@ torch.backends.cudnn.benchmark = False   # will be slower
 
 # data_path = "/media/mynewdrive/bioqa/mnd/intervention/MND-Intervention-1983-06Aug20.json"
 # data_path = "/media/mynewdrive/bioqa/PsyCIPN-II-796-factoid-20s-02112020.json"
+# data_path = "/media/mynewdrive/bioqa/PsyCIPN-II-1984-30s-20012021.json",
 with open(args.data_path) as fin:
     dat = json.load(fin)   
-    
-# # MND
-# dat_train, dat_valid, dat_test = dat['train'], dat['valid'], dat['test']
 
 # PsychoCIPN
-dat_train, dat_valid, dat_test = [], [], []
-for ls in dat:
-    if ls['group'] == 'train':
-        dat_train.append(ls)
-    elif ls['group'] == 'valid':
-        dat_valid.append(ls)
-    else:
-        dat_test.append(ls)
-
-train_contexts, train_questions, train_answers = read_data(dat_train) 
-valid_contexts, valid_questions, valid_answers = read_data(dat_valid) 
- 
- 
+if os.path.basename(args.data_path).split('-')[0] == 'PsyCIPN':
+    dat_train, dat_valid, dat_test = [], [], []
+    for ls in dat:
+        if ls['group'] == 'train':
+            dat_train.append(ls)
+        elif ls['group'] == 'valid':
+            dat_valid.append(ls)
+        else:
+            dat_test.append(ls)
+            
+    ############## For debug only
+    dat_train, dat_valid = dat_train[:100], dat_valid[:10]
+    ##############    
+    train_contexts, train_questions, train_answers, train_pids = read_data(dat_train) 
+    valid_contexts, valid_questions, valid_answers, valid_pids = read_data(dat_valid) 
+else:
+    # MND
+    dat_train, dat_valid, dat_test = dat['train'], dat['valid'], dat['test']
+    train_contexts, train_questions, train_answers = read_data(dat_train) 
+    valid_contexts, valid_questions, valid_answers = read_data(dat_valid) 
+    
+    
 #%% Encodings & Dataset & DataLoader  
 # Define 'Fast' Tokenizer
 if args.pre_wgts == 'distil':
@@ -69,16 +77,20 @@ elif args.pre_wgts == 'pubmed-abs':
 else: # args.pre_wgts == 'bert-base'
     # tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased')
     tokenizer = BertTokenizerFast.from_pretrained('/media/mynewdrive/rob/data/pre_wgts/bert_base')    
- 
+
+
 train_encodings = char2token_encodings(train_contexts, train_questions, train_answers, tokenizer, truncation=True, max_len=512)
 valid_encodings = char2token_encodings(valid_contexts, valid_questions, valid_answers, tokenizer, truncation=True, max_len=512)
-
+# Add pids for list-type   
+if os.path.basename(args.data_path).split('-')[0] == 'PsyCIPN':
+    train_encodings.update({'pids': train_pids})
+    valid_encodings.update({'pids': valid_pids})
+    
 train_dataset = EncodingDataset(train_encodings)
 valid_dataset = EncodingDataset(valid_encodings)
 
 train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
 valid_loader = DataLoader(valid_dataset, batch_size=args.batch_size, shuffle=True)
-
 
 #%% Model & Optimizer & Scheduler
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
@@ -95,14 +107,12 @@ else: # args.pre_wgts == 'bert-base'
     model = BertForQuestionAnswering.from_pretrained('/media/mynewdrive/rob/data/pre_wgts/bert_base')   
     
 model.to(device)
-
-optimizer = AdamW(model.parameters(), lr=5e-5)
+optimizer = AdamW(model.parameters(), lr=2e-5)
 
 # Slanted triangular Learning rate scheduler
 total_steps = len(train_loader) * args.num_epochs // args.accum_step
 warm_steps = int(total_steps * args.warm_frac)
 scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=warm_steps, num_training_steps=total_steps)
-
 
 #%% Train the model
 if os.path.exists(args.exp_dir) == False:
@@ -116,10 +126,24 @@ n_worse = 0
 min_valid_loss = float('inf')
 max_valid_f1 = float('-inf')
 
-for epoch in range(args.num_epochs):   
-    train_scores = train_fn(model, train_loader, optimizer, scheduler, tokenizer, args.clip, args.accum_step, device)
-    valid_scores = valid_fn(model, valid_loader, tokenizer, device)
-
+for epoch in range(args.num_epochs): 
+    if args.type == 'factoid':
+        train_scores = train_fn(model, train_loader, optimizer, scheduler, tokenizer, args.clip, args.accum_step, device)
+        valid_scores = valid_fn(model, valid_loader, tokenizer, device)
+        print("\n\nEpoch {}/{}...".format(epoch+1, args.num_epochs))
+        print('[Train] loss: {0:.3f} | em: {1:.2f}% | f1: {2:.2f}% | prec: {3:.2f}% | rec: {4:.2f}%'.format(
+            train_scores['loss'], train_scores['em']*100, train_scores['f1']*100, train_scores['prec']*100, train_scores['rec']*100))
+        print('[Valid] loss: {0:.3f} | em: {1:.2f}% | f1: {2:.2f}% | prec: {3:.2f}% | rec: {4:.2f}%\n'.format(
+            valid_scores['loss'], valid_scores['em']*100, valid_scores['f1']*100, valid_scores['prec']*100, valid_scores['rec']*100))
+    else:
+        train_scores = train_fn_list(model, train_loader, optimizer, scheduler, tokenizer, args.clip, args.accum_step, device, args.num_answer, args.ans_thres)
+        valid_scores = valid_fn_list(model, valid_loader, tokenizer, device, args.num_answer, args.ans_thres)
+        print("\n\nEpoch {}/{}...".format(epoch+1, args.num_epochs))
+        print('[Train] loss: {0:.3f} | f1: {1:.2f}% | prec: {2:.2f}% | rec: {3:.2f}%'.format(
+            train_scores['loss'], train_scores['f1']*100, train_scores['prec']*100, train_scores['rec']*100))
+        print('[Valid] loss: {0:.3f} | f1: {1:.2f}% | prec: {2:.2f}% | rec: {3:.2f}%\n'.format(
+            valid_scores['loss'], valid_scores['f1']*100, valid_scores['prec']*100, valid_scores['rec']*100))
+        
     # Update output dictionary
     output_dict['prfs'][str('train_'+str(epoch+1))] = train_scores
     output_dict['prfs'][str('valid_'+str(epoch+1))] = valid_scores
@@ -140,12 +164,6 @@ for epoch in range(args.num_epochs):
                                'optim_Dict': optimizer.state_dict()},
                                is_best = is_best, checkdir = args.exp_dir)
     
-    print("\n\nEpoch {}/{}...".format(epoch+1, args.num_epochs))
-    print('[Train] loss: {0:.3f} | em: {1:.2f}% | f1: {2:.2f}% | prec: {3:.2f}% | rec: {4:.2f}%'.format(
-        train_scores['loss'], train_scores['em']*100, train_scores['f1']*100, train_scores['prec']*100, train_scores['rec']*100))
-    print('[Valid] loss: {0:.3f} | em: {1:.2f}% | f1: {2:.2f}% | prec: {3:.2f}% | rec: {4:.2f}%\n'.format(
-        valid_scores['loss'], valid_scores['em']*100, valid_scores['f1']*100, valid_scores['prec']*100, valid_scores['rec']*100))
-    
     # Early stopping             
     # if valid_scores['loss']-min_valid_loss > 0: # args.stop_c1) and (max_valid_f1-valid_scores['f1'] > args.stop_c2):
     #     n_worse += 1
@@ -160,5 +178,5 @@ with open(prfs_path, 'w') as fout:
     json.dump(output_dict, fout, indent=4)
 
 #%% plot
-utils.plot_prfs(prfs_path) 
+# utils.plot_prfs(prfs_path) 
 
